6756a21e70173c079bf78be62bf6643ccb05ffd2
AlexanderSharygin <52005798+AlexanderSharygin@users.noreply.github.com>
2019-07-30 23:40:59 +0300

    g

--- Parser/Program.cs
+++ Parser/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System.IO;
 using System;
 using Parser.Extensions;
-
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using System.Reflection;
 
 namespace Parser
 {
@@ -9,10 +12,12 @@ namespace Parser
     {
         static void Main(string[] args)
         {
+
+
             // No, you didn't read the trello card and didn't follow the suggested approach.
             // Read about System.IO.Path
             string inputTextFilePath = AppDomain.CurrentDomain.BaseDirectory.Replace("\\bin\\Debug\\", "\\Files\\");
-            var inputText = File.ReadAllText(inputTextFilePath + "data.txt");
+           var inputText = File.ReadAllText(inputTextFilePath + "data.txt");
             MyList <JSONObject> JSONObjects = JSONParser.ParseSimpleJSON(inputText);
             ConsolePrinter Printer = new ConsolePrinter();
             Printer.PrintJSONObjectsPropertiesOnConsole(JSONObjects);
