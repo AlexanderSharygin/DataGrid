9c94fa2e722665c43fc0a0440337d2ca41d7e03c
AlexanderSharygin <52005798+AlexanderSharygin@users.noreply.github.com>
2020-11-24 22:32:19 +0300

    New
    NewProject

---
+++ MVCParser/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCParser.Models;
+
+namespace MVCParser.Controllers
+{
+    public class HomeController : Controller
+    {
+        DBModel Db = new DBModel();
+        public ActionResult Index()
+          {
+            return View("Index");
+          }
+        [HttpPost]
+        public ActionResult WorkerSearch()
+        {
+            var jsonResult = Db.Workers.OrderBy(k=>k.Id).Take(100).Select(k => k).ToList();
+
+            return Json(jsonResult, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
