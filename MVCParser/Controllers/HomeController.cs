3b7085996c6dc786cca9fd0a431ea825ad5cf12a
AlexanderSharygin <52005798+AlexanderSharygin@users.noreply.github.com>
2020-12-01 23:15:34 +0300

    Update

--- MVCParser/Controllers/HomeController.cs
+++ MVCParser/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ namespace MVCParser.Controllers
           {
             return View("Index");
           }
+       
         [HttpPost]
         public ActionResult WorkerSearch()
         {
---
+++ MVCParser/Scripts/IndexScripts/AJAXRequest.js
@@ -0,0 +1,29 @@
+﻿
+
+$(document).ready(function () {
+    $.ajax(
+        {
+            type: 'POST',
+            url: "Home/WorkerSearch",
+            dataType: "JSON",
+            contentType: "application/json; charset=utf-8",
+            success: function (response) {
+                let data = response;
+                OnSuccess(data);
+            }
+        })
+});
+let Rows = new Array();
+let Columns = new Set();
+function OnSuccess(data) {
+   
+    for (var i = 0; i < data.length; i++) {
+        let item = data[i];
+        let row = new Map(Object.entries(item));
+
+        for (let j = 0; j < Object.keys(item).length; j++) {
+            Columns.add(Object.keys(item)[j]);
+        }  
+        Rows.push(row);
+    }
+}
