d66b3ec7f5eae4c896c69bf8c505afa0e8996dfd
AlexanderSharygin <52005798+AlexanderSharygin@users.noreply.github.com>
2020-12-02 23:12:08 +0300

    Update

--- MVCParser/Controllers/HomeController.cs
+++ MVCParser/Controllers/HomeController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Json;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 using MVCParser.Models;
+using Newtonsoft.Json;
 
 namespace MVCParser.Controllers
 {
@@ -18,10 +22,11 @@ namespace MVCParser.Controllers
         [HttpPost]
         public ActionResult WorkerSearch()
         {
-            var jsonResult = Db.Workers.OrderBy(k=>k.Id).Take(100).Select(k => k).ToList();
-
+           
+            var jsonResult = Db.Workers.OrderBy(k=>k.Id).Take(100).Select(k => k).ToList();         
             return Json(jsonResult, JsonRequestBehavior.AllowGet);
         }
 
     }
 }
+
--- MVCParser/Scripts/IndexScripts/AJAXRequest.js
+++ MVCParser/Scripts/IndexScripts/AJAXRequest.js
@@ -26,4 +26,40 @@ function OnSuccess(data) {
         }  
         Rows.push(row);
     }
+    let table = document.getElementById('table');
+    for (item of Columns)
+    {
+        let th = document.createElement('th');
+        th.innerHTML = item;
+        table.appendChild(th);
+    }
+    for (let row of Rows)
+    {
+        let tr = document.createElement('tr');
+        for (columnName of Columns) {
+            let value = row.get(columnName);
+            let td = document.createElement('td');
+            if (value != undefined)
+            {
+                if (value.length > columnName.length * 3)
+                {
+                    let subValue = value.slice(0, columnName.length * 3) + '...';
+                    value = subValue;
+                }
+
+                if (value.toString().includes("Date"))
+                {
+                    let startIndex = value.indexOf('(')+1;
+                    let subValue = value.slice(startIndex);
+                    subValue = subValue.slice(0, subValue.length - 2);
+                    let date = new Date(parseInt(subValue));
+                    value = date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear();
+                  
+                }
+            }
+                td.innerHTML = value;          
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
 }
