57020ffe4631f5f97cda5b742746979de5a919f5
AlexanderSharygin <52005798+AlexanderSharygin@users.noreply.github.com>
2020-12-05 21:55:37 +0300

    Update

--- MVCParser/Controllers/HomeController.cs
+++ MVCParser/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Json;
@@ -13,19 +14,69 @@ namespace MVCParser.Controllers
 {
     public class HomeController : Controller
     {
+
+
         DBModel Db = new DBModel();
+        List<Column> Columns = new List<Column>();
+
         public ActionResult Index()
-          {
+        {
+           
+          
             return View("Index");
-          }
-       
+
+        }
+        
+
         [HttpPost]
         public ActionResult WorkerSearch()
         {
-           
-            var jsonResult = Db.Workers.OrderBy(k=>k.Id).Take(100).Select(k => k).ToList();         
+
+            var jsonResult = Db.Workers.OrderBy(k => k.Id).Take(100).Select(k => k).ToList();
             return Json(jsonResult, JsonRequestBehavior.AllowGet);
         }
+
+        public ActionResult GetAllColumns()
+        {
+            GetColumnsInfo();
+
+           
+            return Json(Columns, JsonRequestBehavior.AllowGet);
+
+        }
+        private void GetColumnsInfo()
+        {
+          
+            var data = Db.Workers.FirstOrDefault();
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(data);
+            foreach (PropertyDescriptor property in properties)
+            {
+                bool isExist = false;
+                for (int i = 0; i < Columns.Count; i++)
+                {
+                    if (Columns[i].Name == property.Name)
+                    {
+                        isExist = true;
+                        break;
+                    }
+                }
+                if (!isExist)
+                {
+                    Column col = new Column();
+                    col.Name = property.Name;
+                    col.Type = property.PropertyType.ToString();
+                    Columns.Add(col);
+                }
+            }
+        }
+
+
+    }
+    public class Column
+    {
+        public String Name { get; set; }
+        public string Type { get; set; }
+      
 
     }
 }
--- MVCParser/Scripts/IndexScripts/AJAXRequest.js
+++ MVCParser/Scripts/IndexScripts/AJAXRequest.js
@@ -4,17 +4,58 @@ $(document).ready(function () {
     $.ajax(
         {
             type: 'POST',
-            url: "Home/WorkerSearch",
+            url: "Home/GetAllColumns",
             dataType: "JSON",
             contentType: "application/json; charset=utf-8",
             success: function (response) {
                 let data = response;
-                OnSuccess(data);
+                OnColumnsLoad(data);
             }
         })
 });
 let Rows = new Array();
 let Columns = new Set();
+let ColumnsToLoad = new Array();
+function OnColumnsLoad(data) {
+
+    let columns = document.getElementById('columns');
+    for (var i = 0; i < data.length; i++) {
+        let item = data[i];
+        let label = document.createElement('label');
+        label.classList.add('container');
+        label.innerHTML = item.Name;
+        let input = document.createElement('input');
+        input.type = 'checkbox';
+        let span = document.createElement('span');
+        span.classList.add('checkmark');
+        label.appendChild(input);
+        label.appendChild(span);
+        columns.appendChild(label);
+
+    }
+    let button = document.createElement('input');
+    button.type = 'button';
+    button.classList.add('button');
+    button.value = 'Load';
+    columns.appendChild(button);
+    button.onclick = function ()
+    {
+        ColumnsToLoad = [];
+        var items = document.querySelectorAll('input[type="checkbox"]');
+        for (let item of items)
+        {
+            if (item.checked)
+            {
+                ColumnsToLoad.push(item.parentElement.innerText);
+
+            }
+        }
+
+    }
+
+}
+
+
 function OnSuccess(data) {
    
     for (var i = 0; i < data.length; i++) {
